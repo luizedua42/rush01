80ed4454ec2f3c38ed8bca485d170da8646a9ee1
Luiz Eduardo Adami <zeexem@hotmail.com>
2023-05-31 18:06:42 -0300

    line_checker

--- rush-01.c
+++ rush-01.c
@@ -6,12 +6,14 @@
 /*   By: luizedua <luizedua@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 15:24:40 by luizedua          #+#    #+#             */
-/*   Updated: 2023/05/30 14:11:54 by vcedraz-         ###   ########.fr       */
+/*   Updated: 2023/05/31 17:33:01 by luizedua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdio.h>
 
+int		lines_are_valid(char **args);
 void	print_borders(char **matrix, int width);
 char	**init_matrix(char **argv);
 void	free_matrix(char **matrix, int height);
@@ -23,6 +25,11 @@ int	main(int argc, char **argv)
 	if (1 == argc)
 		return (1);
 	matrix = init_matrix(argv);
-	print_borders(matrix, 4);
-	free_matrix(matrix, 4);
+	if (!lines_are_valid(matrix))
+	{
+		printf("error\n");
+		free_matrix(matrix, 4);
+		return (0);
+	}
+	// print_borders(matrix, 4);
 }
